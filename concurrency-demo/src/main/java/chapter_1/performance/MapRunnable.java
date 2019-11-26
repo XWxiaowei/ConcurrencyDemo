6a077cb57cd077e559356ed37e7cb8d8c8a021c1
XWxiaowei <2809641033@qq.com>
2019-11-26 20:51:54 +0800

    代码提交

--- concurrency-demo/src/main/java/chapter_1/performance/MapRunnable.java
+++ concurrency-demo/src/main/java/chapter_1/performance/MapRunnable.java
@@ -84,27 +84,18 @@ public class MapRunnable {
 //        );
 //        //线程的最大空闲时间，超出这个时间将进行回收
 //        se.allowCoreThreadTimeOut(true);
-//        ThreadFactory threadFactory = new ThreadFactoryBuilder().setNameFormat("测试-%d").build();
-//        ExecutorService se = new ThreadPoolExecutor(10, 300, 1, TimeUnit.SECONDS,
-//                new LinkedBlockingQueue<>(), threadFactory, new ThreadPoolExecutor.AbortPolicy());
+        ThreadFactory threadFactory = new ThreadFactoryBuilder().setNameFormat("测试-%d").build();
+        ExecutorService se = new ThreadPoolExecutor(10, 300, 1, TimeUnit.SECONDS,
+                new LinkedBlockingQueue<>(), threadFactory, new ThreadPoolExecutor.AbortPolicy());
 //        ExecutorService se=new ThreadPoolExecutor(5, 2*threadCount,
 //                1L, TimeUnit.SECONDS,
 //                new SynchronousQueue<Runnable>());
-        ExecutorService se = Executors.newCachedThreadPool();
         //同时开启2*threadCount个读写线程
         for (int i = 0; i < threadCount; i++) {
             se.execute(writeRun);
 //            se.execute(readrun);
 //            new Thread(writeRun,"每天的一天").start();
         }
-//        //统计
-//        try {
-//            cyclicBarrier.await();
-//        } catch (InterruptedException e) {
-//            e.printStackTrace();
-//        } catch (BrokenBarrierException e) {
-//            e.printStackTrace();
-//        }
         map.clear();
         //所有线程都执行完成之后，才会跑到这一步
         long duration = (System.currentTimeMillis() - starttime);
---
+++ concurrency-demo/src/main/java/chapter_2/MyThreadPool.java
@@ -0,0 +1,9 @@
+package chapter_2;
+
+/**
+ * @author xiang.wei
+ * @date 2019/11/26 20:32
+ */
+public class MyThreadPool {
+
+}
